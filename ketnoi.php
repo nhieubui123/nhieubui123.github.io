e97d7337cca66f91612da10a4f99294eb989875b
nhieubui123 <54296881+nhieubui123@users.noreply.github.com>
2019-08-20 22:20:53 +0700

    Add files via upload

---
+++ ketnoi.php
@@ -0,0 +1,9 @@
+<?php
+
+mysql_connect("localhost","root","123456789");
+mysql_select_db("shop_quanao");
+mysql_query('SET NAMES "UTF8"');
+//$knknkn=mysql_connect("localhost","root","123456789");
+$ten_danh_dau="abcde___";
+//echo $ten_danh_dau."<hr>";
+?>
---
+++ ketnoi_host.php
@@ -0,0 +1,9 @@
+<?php
+
+mysql_connect("sql311.byethost8.com","b8_10587085","123456");
+mysql_select_db("b8_10587085_thoitrangblue");
+mysql_query('SET NAMES "UTF8"');
+//$knknkn=mysql_connect("localhost","root","");
+$ten_danh_dau="abcde___";
+//echo $ten_danh_dau."<hr>";
+?>
